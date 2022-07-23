72cf87e0646e1a2cb21770015eb5e70cea32c1c9
downTwo <109847458+downTwo@users.noreply.github.com>
2022-07-23 22:12:16 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-wget https://nyaa.si/download/1555471.torrent
+wget https://sukebei.nyaa.si/download/3694974.torrent
 aria2c -x 64 --bt-remove-unselected-file=true --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 
-rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/animation_1/Lilith-Raws/[Lilith-Raws] 杜鵑小鬧劇"
+rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/AutoDownFilesTo
+/AutoDownFilesTo
+/download/ "onedrive:/animation_comic"
