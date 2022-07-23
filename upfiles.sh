ed557460539ff2e376fc197b4a99149c3a03cf17
downTwo <109847458+downTwo@users.noreply.github.com>
2022-07-23 22:20:46 +0800

    Update upfiles.sh

--- upfiles.sh
+++ upfiles.sh
@@ -4,4 +4,4 @@ aria2c -x 64 --bt-remove-unselected-file=true --seed-time=0 --bt-tracker-connect
 
 rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/AutoDownFilesToDrive
 /AutoDownFilesToDrive
-/download/ "onedrive:/animation_comic"
+/download/ "onedrive:/animation_comic/"
