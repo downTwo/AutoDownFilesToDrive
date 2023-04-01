530f06893c67afb66354d90b215b542c15e7717c
downTwo <animation_comic@shthey.onmicrosoft.com>
2023-04-01 06:00:39 +0000

    (同人CG集) [朝風の雫 (朝風あびす)] サークル朝風の雫支援者様向けミニCG集まとめ vol.4

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://sukebei.nyaa.si/download/3854825.torrent
+wget https://sukebei.nyaa.si/download/3854773.torrent
 aria2c -x 64 --bt-remove-unselected-file=true --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 
-rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/AutoDownFilesToDrive/AutoDownFilesToDrive/download/ "onedrive:/animation_comic/milannews/Doujinshi"
+rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/AutoDownFilesToDrive/AutoDownFilesToDrive/download/ "onedrive:/animation_comic/milannews/Pictures"
